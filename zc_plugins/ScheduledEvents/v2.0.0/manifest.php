eb821fb561c824bb29db9d82b1dcc97795b5474c
dbltoe <dbltoe@gmail.com>
2026-07-15 23:45:42 -0500

    Add v223 to declared supported Zen Cart versions

--- zc_plugins/ScheduledEvents/v2.0.0/manifest.php
+++ zc_plugins/ScheduledEvents/v2.0.0/manifest.php
@@ -8,7 +8,7 @@ return [
     'pluginDescription' => 'Displays upcoming scheduled events (shows, fairs, expos, etc.) on their own storefront page, with an optional link in the Information sidebox or its own sidebox (simple link or slider) placeable via the Layout Boxes Controller. Events are managed from a dedicated admin screen. Replaces the older, non-encapsulated version of this plugin; requires Zen Cart 2.0.0 or later.',
     'pluginAuthor' => 'dbltoe',
     'pluginId' => 0,
-    'zcVersions' => ['v200', 'v201', 'v210', 'v221', 'v222'],
+    'zcVersions' => ['v200', 'v201', 'v210', 'v221', 'v222', 'v223'],
     'changelog' => 'changelog.txt',
     'github_repo' => '',
     'pluginGroups' => [],
